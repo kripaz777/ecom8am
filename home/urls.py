fbff6f1c493ee10dbde96cdf2a1e9a5a019407c5
aayusha-aigeal <aayush.adhikari@aigeal.com>
2022-04-26 08:24:24 +0545

    subcategory

--- home/urls.py
+++ home/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('',HomeView.as_view(),name = 'home'),
     path('detail/<slug>',ProductDetailView.as_view(),name = 'detail'),
     path('category/<slug>',CategoryView.as_view(),name = 'category'),
-
+    path('subcategory/<slug>',SubCategoryView.as_view(),name = 'subcategory'),
 
 ]
